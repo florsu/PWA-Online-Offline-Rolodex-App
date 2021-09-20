bdfbee2e7fbcb5dcdc4be02c59a3c4ff5beb5f54
Christian Flores <christiangflores0824@gmail.com>
2021-09-19 20:42:11 -0700

    updated URL

--- scripts/apiFunctions.js
+++ scripts/apiFunctions.js
@@ -5,9 +5,10 @@ const storedUserData = JSON.parse(localStorage.getItem('userData'))
 
 export const getListOfUsers = async () => {
     if (localStorage.length === 0) {
-        const URL = 'https://jsonplaceholder.typicode.com/users'
+        const URL = 'https://my-json-server.typicode.com/florsu/PWA-Online-Offline-Rolodex-App/clients'
         let res = await fetch(URL)
         let json = await res.json()
+        console.log(JSON.stringify(json))
 
         localStorage.setItem('userData', JSON.stringify(json))
         createRolodexList(storedUserData)
